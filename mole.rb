fec53bdeecd6e71b4b8dd9f5175fc7e1b185b9c2
yuki-mtmr <yuki.matsumori.exam@gmail.com>
2020-01-16 19:45:15 +0900

    add mole_game

---
+++ mole.rb
@@ -0,0 +1,75 @@
+require 'gosu'
+
+class WhackaMole < Gosu::Window
+  def initialize
+    super(800, 600)
+    self.caption = "Whack 'a Mole"
+    @image = Gosu::Image.new('images_mole/mole.png')
+    @x = 200
+    @y = 200
+    @width = 100
+    @height = 75
+    @velosity_x = 5
+    @velosity_y = 5
+    @visible = 0
+    @hammer = Gosu::Image.new('images_mole/hammer.png')
+    @hit = 0
+    @font = Gosu::Font.new(30)
+    @score = 0
+    @playing = true
+  end
+
+  def draw
+    if @visible > 0
+      @image.draw(@x - @width / 2, @y - @height / 2, 1)
+    end
+    @hammer.draw(mouse_x - 25, mouse_y - 39, 1)
+    if @hit == 0
+      c = Gosu::Color::NONE
+    elsif @hit == 1
+      c = Gosu::Color::GREEN
+    elsif @hit == -1
+      c = Gosu::Color::RED
+    end
+    draw_quad(0, 0, c, 800, 0, c, 800, 600, c, 0, 600, c)
+    @hit = 0
+    @font.draw(@score.to_s, 650, 50, 2)
+    @font.draw(@time_left.to_s, 50, 50, 2)
+    unless @playing
+      @font.draw("Game Over!!!", 300, 300, 3)
+      @visible = 20
+      @time_left = 0
+    end
+  end
+
+  def update
+    if @playing
+      @x += @velosity_x
+      @y += @velosity_y
+      @velosity_x *= -1 if @x + @width / 2 > 800 || @x - @width / 2 < 0
+      @velosity_y *= -1 if @y + @height / 2 > 600 || @y - @height / 2 < 0
+      @visible -= 1
+      @visible = 50 if @visible < -10 && rand < 0.01
+      @time_left = (60 - (Gosu.milliseconds / 1000))
+      @playing = false if @time_left < 1
+    end
+  end
+
+  def button_down(id)
+    if @playing
+      if (id == Gosu::MsLeft)
+        if Gosu.distance(mouse_x, mouse_y, @x, @y) < 60 && @visible >= 0
+          @hit = 1
+          @score += 10
+        else
+          @hit = -1
+          @score -= 3
+        end
+      end
+    end
+  end
+
+end
+
+window = WhackaMole.new
+window.show
--- space.rb
+++ space.rb
@@ -22,33 +22,41 @@ class Tutorial < Gosu::Window
     @weed = Gosu::Image.load_tiles("images/weed.png", 25, 25)
     @stars = Array.new
     @weeds = Array.new
+    @visible = 0
+    @playing = true
 
     @font = Gosu::Font.new(20)
 
   end
 
   def update
-    if Gosu.button_down? Gosu::KbLeft or Gosu::button_down? Gosu::GpLeft
-      @player.turn_left
-    end
+    if @playing
+      if Gosu.button_down? Gosu::KbLeft or Gosu::button_down? Gosu::GpLeft
+        @player.turn_left
+      end
 
-    if Gosu.button_down? Gosu::KbRight or Gosu::button_down? Gosu::GpRight
-      @player.turn_right
-    end
+      if Gosu.button_down? Gosu::KbRight or Gosu::button_down? Gosu::GpRight
+        @player.turn_right
+      end
 
-    if Gosu.button_down? Gosu::KbUp or Gosu::button_down? Gosu::GpButton0
-      @player.accelerate
-    end
-    @player.move
-    @player.collect_stars(@stars)
-    @player.collect_weeds(@weeds)
+      if Gosu.button_down? Gosu::KbUp or Gosu::button_down? Gosu::GpButton0
+        @player.accelerate
+      end
+      @player.move
+      @player.collect_stars(@stars)
+      @player.collect_weeds(@weeds)
 
-    if rand(100) < 4 and @stars.size < 25
-      @stars.push(Star.new(@star_anim))
-    end
+      if rand(100) < 4 and @stars.size < 25
+        @stars.push(Star.new(@star_anim))
+      end
 
-    if rand(1000) < 1 and @weeds.size < 25
-      @weeds.push(Weed.new(@weed))
+      if rand(1000) < 1 and @weeds.size < 25
+        @weeds.push(Weed.new(@weed))
+      end
+      @visible -= 1
+      @visible = 50 if @visible < -10 && rand < 0.01
+      @time_left = (60 - (Gosu.milliseconds / 1000))
+      @playing = false if @time_left < 1
     end
   end
 
@@ -56,8 +64,17 @@ class Tutorial < Gosu::Window
     @player.draw
     @background_image.draw(0, 0, ZOrder::BACKGROUND)
     @stars.each { |star| star.draw }
-    @weeds.each { |weed| weed.draw }
+    if @visible > 0
+      @weeds.each { |weed| weed.draw }
+    end
     @font.draw("Score: #{@player.score}", 10, 10, ZOrder::UI, 1.0, 1.0, Gosu::Color::WHITE)
+    @font.draw(@time_left.to_s, 600, 10, 2)
+
+    unless @playing
+      @font.draw("Game Over!!!", 260, 200, 3)
+      @visible = 20
+      @time_left = 0
+    end
   end
 
   def button_down(id)
